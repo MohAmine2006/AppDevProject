aa7341d87395803b7d5a1af56040d2af8c7a942d
Daiyan Abrar Samin <daiyan03samin@gmail.com>
2025-11-21 14:37:33 -0500

    foreign key
    update foreign key references and add value to the tables

--- expense.sql
+++ expense.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2025 at 07:56 PM
+-- Generation Time: Nov 21, 2025 at 08:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,6 +32,13 @@ CREATE TABLE `categories` (
   `categoryName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categoryID`, `categoryName`) VALUES
+(1000, 'Transportation');
+
 -- --------------------------------------------------------
 
 --
@@ -40,13 +47,20 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `transactions` (
   `transactionID` int(10) NOT NULL,
-  `amount` int(10) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
   `type` varchar(20) NOT NULL,
   `categoryID` int(10) NOT NULL,
   `userID` int(10) NOT NULL,
   `date` date NOT NULL,
   `note` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transactionID`, `amount`, `type`, `categoryID`, `userID`, `date`, `note`) VALUES
+(100, 50.00, 'Expense', 1000, 1, '2025-11-20', 'Purchase monthly metro card');
 
 -- --------------------------------------------------------
 
@@ -62,6 +76,13 @@ CREATE TABLE `users` (
   `lastName` varchar(40) NOT NULL,
   `phoneNum` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `firstName`, `lastName`, `phoneNum`) VALUES
+(1, 'JohnS', 'jhonny123', 'John', 'Smith', '(514) 256-1092');
 
 --
 -- Indexes for dumped tables
@@ -95,19 +116,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transactionID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `transactionID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -117,8 +138,8 @@ ALTER TABLE `users`
 -- Constraints for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD CONSTRAINT `categoryFK` FOREIGN KEY (`categoryID`) REFERENCES `transactions` (`transactionID`),
-  ADD CONSTRAINT `userFK` FOREIGN KEY (`userID`) REFERENCES `transactions` (`transactionID`);
+  ADD CONSTRAINT `categoryFK` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`categoryID`),
+  ADD CONSTRAINT `userFK` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
