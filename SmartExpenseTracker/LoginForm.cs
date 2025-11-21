50eac2754a6d7f730c6114365fef64245441e292
Daiyan Abrar Samin <daiyan03samin@gmail.com>
2025-11-21 16:54:43 -0500

    User login
    Implemented and modified LoginForm to achieve user login

--- SmartExpenseTracker/LoginForm.cs
+++ SmartExpenseTracker/LoginForm.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace SmartExpenseTracker
 {
     public partial class LoginForm : Form
     {
         public string LoggedInUser { get; private set; }
+        string mySqlConn = "Server=127.0.0.1;Database=expense;User=root;Password=";
 
         public LoginForm()
         {
@@ -24,17 +27,64 @@ namespace SmartExpenseTracker
             string username = usernameTextBox.Text;
             string password = passwordTextBox.Text;
 
-            if (username == "Admin" && password == "1234")
+            // Tried to hash password but couldn't get it to work with the database
+            //string hashedInput = SecurityHelper.HashPassword(password); 
+
+            try
             {
-                LoggedInUser = username;
-                this.DialogResult = DialogResult.OK; 
-                this.Close();
+                using (MySqlConnection conn = new MySqlConnection(mySqlConn))
+                {
+                    // Validate both username and password are provided
+                    if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+                    {
+                        MessageBox.Show("Please enter both username and password.", "Input Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        //return;
+                    }
+                    else
+                    {
+                        // MySQL query to check for matching username and password
+                        conn.Open();
+                        string query = "SELECT userID FROM users WHERE username=@username AND password=@password";
+                        MySqlCommand cmd = new MySqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            LoggedInUser = username;
+                            this.DialogResult = DialogResult.OK;
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid username or password", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Invalid username or password", "Error", 
+                MessageBox.Show("An error occurred: " + ex.Message, "Error",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
+
+
+            //if (username == "Admin" && password == "1234")
+            //{
+            //    LoggedInUser = username;
+            //    this.DialogResult = DialogResult.OK; 
+            //    this.Close();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Invalid username or password", "Error", 
+            //        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
         }
     }
 }
---
+++ SmartExpenseTracker/SecurityHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartExpenseTracker
+{
+    public static class SecurityHelper
+    {
+        public static string HashPassword(string password)
+        {
+            // Simple hash implementation for user passwords
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
